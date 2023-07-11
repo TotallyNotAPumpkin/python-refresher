7a225efec27a2bf0ae8727afe47b1fdbd199bc60
TotallyNotAPumpkin <leopardcloudew@gmail.com>
2023-07-11 17:20:51 -0400

    Created Bank class and methods

---
+++ bank.py
@@ -0,0 +1,32 @@
+class Bank:
+    def __init__(self, name, number, bal = 0):
+        self.name = name
+        self.number = number
+        self.bal = bal
+
+    def wd(self, accnum, amt):
+        if accnum == self.number:
+            if self.bal >= amt:
+                self.bal -= amt
+            else: 
+                print(f"You cannot withdraw more than your current balance: {self.bal}")
+        else:
+            print("Incorrect account number.")
+
+    def dep(self, accnum, amt):
+        if accnum == self.number:
+            self.bal += amt
+        else:
+            print("Incorrect account number.")
+
+    def balance(self, accnum):
+        if accnum == self.number:
+            print(self.bal)
+        else:
+            print("Incorrect account number.")
+
+emily = Bank("Emily Wang", 1)
+emily.balance(1)
+emily.dep(30)
+emily.balance(1)
+emily.wd(20)

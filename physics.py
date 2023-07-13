6eeb68a484c97698b0e44d953fc20b651c9a1275
TotallyNotAPumpkin <leopardcloudew@gmail.com>
2023-07-13 11:39:28 -0400

    i didi stuff

--- physics.py
+++ physics.py
@@ -1,4 +1,6 @@
 g = 9.81 # m/s^2
+atm_pressure = 101325 # Pa
+water_density = 1000 # kg/m^3
 
 def calculate_buoyancy(V, density_fluid):
     """Compute and returns the buoyancy of an object in a fluid.
@@ -8,7 +10,7 @@ def calculate_buoyancy(V, density_fluid):
     Returns: 
         Fb (float): The buoyant force applied - calulated using archimedes principal (N)
     """
-    if V or density_fluid > 0:
+    if V < 0 or density_fluid < 0:
         raise ValueError("Inputs must be positive.")
     Fb = density_fluid * V * g
     return Fb
@@ -21,10 +23,11 @@ def will_it_float(V, mass):
     Returns:
         (boolean): True if floats, False if sinks
     """
-
-    if calculate_buoyancy(V, 1000) > (mass * g):
+    if V < 0 or mass < 0:
+        raise ValueError("Inputs must be positive.")
+    if calculate_buoyancy(V, water_density) > (mass * g):
         return True
-    if calculate_buoyancy(V, 1000) < (mass * g):
+    if calculate_buoyancy(V, water_density) < (mass * g):
         return False
     else:
         pass
@@ -36,12 +39,13 @@ def calculate_pressure(depth):
     Returns:
         pressure (float): the pressure exerted on the object by the fluid (Pa)
     """
-    density_water = 1000 # kg/m^3
-    pressure = density_water * g * depth
-    return pressure
+    if depth < 0:
+        raise ValueError("Input must be positive.")
+    pressure = water_density * g * depth + atm_pressure
+    return pressure # Pa
 
 if __name__ == "__main__":
     # Trial
-    print(calculate_buoyancy(1, 1000))
+    print(calculate_buoyancy(1, water_density))
     print(will_it_float(1, 999))
     print(calculate_pressure(100))
--- test_physics.py
+++ test_physics.py
@@ -5,7 +5,8 @@ import physics
 class TestPhysics(unittest.TestCase):
     def test_calculate_buoyancy(self):
         self.assertEqual(physics.calculate_buoyancy(1, 1000), 9810.0)
-
+        with self.assertRaises(ValueError):
+            physics.calculate_buoyancy(-30, 1000)
 
 
 
